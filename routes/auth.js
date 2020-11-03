c479cbfca7f7cd33c4f6daba2539a31580fa5120
Jeremy Langevin <langevinj832@gmail.com>
2020-11-03 11:27:46 -0500

    users and messages routes created

--- routes/auth.js
+++ routes/auth.js
@@ -1,51 +1,42 @@
 
-const express = require("express");
-const router = new express.Router();
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const Router = require("express").Router();
+const router = new Router();
 
+const User = require("../models/user");
 const ExpressError = require("../expressError");
 const { SECRET_KEY } = require("../config");
 
-/** POST /login - login: {username, password} => {token}
- *
- * Make sure to update their last-login!
- *
- **/
-
+ /**login: {username, password} => {token} */
 router.post("/login", async function(req, res, next) {
     try{
         const { username, password } = req.body;
         const result = await User.authenticate(username, password);
+
         if(result === true){
             let token = jwt.sign({ username }, SECRET_KEY);
             User.updateLoginTimestamp(username);
             return res.json({ token })
-        }   
-        throw new ExpressError("Invalid username/password", 400)
+        } else {
+            throw new ExpressError("Invalid username/password", 400)
+        }
     } catch (e) {
         return next(e);
     }
 });
 
-/** POST /register - register user: registers, logs in, and returns token.
+/**  register user: registers, logs in, and returns token.
  *
  * {username, password, first_name, last_name, phone} => {token}.
- *
- *  Make sure to update their last-login!
  */
 
 router.post("/register", async function (req, res, next) {
     try{
-        const { username, password, first_name, last_name, phone} = req.body;
-        const result = await User.register({ username, password, first_name, last_name, phone});
+        const { username } = await User(req.body);
+        let token = jwt.sign({ username }, SECRET_KEY);
+        User.updateLoginTimestamp(username);
+        return res.json({ token });
 
-        if(result){
-            let token = jwt.sign({ username }, SECRET_KEY);
-            User.updateLoginTimestamp(username);
-            return res.json({ token });
-        }
-        throw new ExpressError("Invalid username/password", 400)
     } catch (e) {
         return next(e);
     }
--- routes/messages.js
+++ routes/messages.js
@@ -1,4 +1,10 @@
-/** GET /:id - get detail of message.
+const Router = require("express").Router;
+const Message = require("../models/message")
+const {ensureLoggedIn} = require("../middleware/auth");
+const ExpressError = require("../expressError");
+const router = new Router();
+
+/**- get detail of message.
  *
  * => {message: {id,
  *               body,
@@ -6,25 +12,58 @@
  *               read_at,
  *               from_user: {username, first_name, last_name, phone},
  *               to_user: {username, first_name, last_name, phone}}
- *
- * Make sure that the currently-logged-in users is either the to or from user.
- *
  **/
 
+router.get("/:id", ensureLoggedIn, async function(req, res, next) {
+    try {
+        const username = req.user.username;
+        const message = await Message.get(req.params.id)
+        
+        if(message.from_user.username !== username && message.to_user.username !== username){
+            throw new ExpressError("Cannot read this message", 401);
+        }
+        return res.json({message})
+    } catch(e) {
+        return next(e);
+    }
+});
 
-/** POST / - post message.
+/** - post message.
  *
- * {to_username, body} =>
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
 
+router.post("/", ensureLoggedIn, async function(req, res, next) {
+    try{
+        const message = await Message.create({
+            from_username: req.user.username,
+            to_username: req.bod.to_username,
+            body: req.body.body
+        });
+        return res.json({message: message});
+    } catch(e) {
+        return next(e)
+    }
+});
 
-/** POST/:id/read - mark message as read:
+
+/** mark message as read:
  *
  *  => {message: {id, read_at}}
  *
- * Make sure that the only the intended recipient can mark as read.
- *
  **/
+router.post("/:id/read", ensureLoggedIn, async function(req, res, next) {
+    try {
+        const result = await Message.get(req.params.id)
+        if(result.to_user.username !== req.user.username){
+            throw new ExpressError("You cannot mark this message read", 401)
+        }
+        let message = await Message.markRead(req.params.id)
+        return res.json({message});
+    } catch (e) {
+        return next(e);
+    }
+});
 
+module.exports = router;
